0b39aa69d33357c325617891621b08b709d595c1
li <im.yuqlee@gmail.com>
2025-02-05 22:13:05 +0800

    test keystore

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const gikHook = require('./models')
 
+const { client } = require('./api')
+const { MixinApi, base64RawURLEncode } = require('@mixin.dev/mixin-node-sdk');
+const { v4 } = require('uuid');
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true)
   res.header('Access-Control-Allow-Origin', '*')
@@ -19,6 +23,13 @@ app.use(bodyParser.json({limit : "2100000kb"}))
 
 
 app.get('/api/', async (req, res) => {
+  let base64data = base64RawURLEncode('hi githook');
+  client.message.sendLegacy({
+    conversation_id: '9451292c-c81c-4574-961a-ce9075e32400',
+    message_id: v4(),
+    category: 'PLAIN_POST',
+    data_base64: base64data,
+  })
   return res.json({ data: 'ok' })
 })
 
